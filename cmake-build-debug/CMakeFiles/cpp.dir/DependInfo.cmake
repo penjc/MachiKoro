9e1fd64b2ec8eacc2cbc8b058d97a898adc55da2
penjc <2686728826@qq.com>
2023-04-04 01:20:27 +0800

    cmakelist update

---
+++ cmake-build-debug/CMakeFiles/cpp.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/peng/cpp/MachiKoro/Banque.cpp" "CMakeFiles/cpp.dir/Banque.cpp.o" "gcc" "CMakeFiles/cpp.dir/Banque.cpp.o.d"
+  "/Users/peng/cpp/MachiKoro/Cartes.cpp" "CMakeFiles/cpp.dir/Cartes.cpp.o" "gcc" "CMakeFiles/cpp.dir/Cartes.cpp.o.d"
+  "/Users/peng/cpp/MachiKoro/Cartes_bleu.cpp" "CMakeFiles/cpp.dir/Cartes_bleu.cpp.o" "gcc" "CMakeFiles/cpp.dir/Cartes_bleu.cpp.o.d"
+  "/Users/peng/cpp/MachiKoro/Cartes_jaune.cpp" "CMakeFiles/cpp.dir/Cartes_jaune.cpp.o" "gcc" "CMakeFiles/cpp.dir/Cartes_jaune.cpp.o.d"
+  "/Users/peng/cpp/MachiKoro/Cartes_rouge.cpp" "CMakeFiles/cpp.dir/Cartes_rouge.cpp.o" "gcc" "CMakeFiles/cpp.dir/Cartes_rouge.cpp.o.d"
+  "/Users/peng/cpp/MachiKoro/Cartes_vert.cpp" "CMakeFiles/cpp.dir/Cartes_vert.cpp.o" "gcc" "CMakeFiles/cpp.dir/Cartes_vert.cpp.o.d"
+  "/Users/peng/cpp/MachiKoro/Cartes_violet.cpp" "CMakeFiles/cpp.dir/Cartes_violet.cpp.o" "gcc" "CMakeFiles/cpp.dir/Cartes_violet.cpp.o.d"
+  "/Users/peng/cpp/MachiKoro/Deck.cpp" "CMakeFiles/cpp.dir/Deck.cpp.o" "gcc" "CMakeFiles/cpp.dir/Deck.cpp.o.d"
+  "/Users/peng/cpp/MachiKoro/Jeu.cpp" "CMakeFiles/cpp.dir/Jeu.cpp.o" "gcc" "CMakeFiles/cpp.dir/Jeu.cpp.o.d"
+  "/Users/peng/cpp/MachiKoro/MachiKoro.cpp" "CMakeFiles/cpp.dir/MachiKoro.cpp.o" "gcc" "CMakeFiles/cpp.dir/MachiKoro.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/cpp.dir/cmake_clean.cmake
@@ -0,0 +1,29 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpp.dir/Banque.cpp.o"
+  "CMakeFiles/cpp.dir/Banque.cpp.o.d"
+  "CMakeFiles/cpp.dir/Cartes.cpp.o"
+  "CMakeFiles/cpp.dir/Cartes.cpp.o.d"
+  "CMakeFiles/cpp.dir/Cartes_bleu.cpp.o"
+  "CMakeFiles/cpp.dir/Cartes_bleu.cpp.o.d"
+  "CMakeFiles/cpp.dir/Cartes_jaune.cpp.o"
+  "CMakeFiles/cpp.dir/Cartes_jaune.cpp.o.d"
+  "CMakeFiles/cpp.dir/Cartes_rouge.cpp.o"
+  "CMakeFiles/cpp.dir/Cartes_rouge.cpp.o.d"
+  "CMakeFiles/cpp.dir/Cartes_vert.cpp.o"
+  "CMakeFiles/cpp.dir/Cartes_vert.cpp.o.d"
+  "CMakeFiles/cpp.dir/Cartes_violet.cpp.o"
+  "CMakeFiles/cpp.dir/Cartes_violet.cpp.o.d"
+  "CMakeFiles/cpp.dir/Deck.cpp.o"
+  "CMakeFiles/cpp.dir/Deck.cpp.o.d"
+  "CMakeFiles/cpp.dir/Jeu.cpp.o"
+  "CMakeFiles/cpp.dir/Jeu.cpp.o.d"
+  "CMakeFiles/cpp.dir/MachiKoro.cpp.o"
+  "CMakeFiles/cpp.dir/MachiKoro.cpp.o.d"
+  "cpp"
+  "cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
